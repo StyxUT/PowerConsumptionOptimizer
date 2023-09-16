d7e4244c7af1bd82fc19aa7d5341db03417673a0
Eric Barnes <ebarnes@marketstar.com>
2023-09-16 09:39:54 -0600

    ensure DetermineChargingPriority has been called before starting charge monitoring

--- PowerConsumptionOptimizer/ConsumptionOptimizer.cs
+++ PowerConsumptionOptimizer/ConsumptionOptimizer.cs
@@ -67,13 +67,14 @@ namespace PowerConsumptionOptimizer
                         }
                     });
 
+                    DetermineChargingPriority(); // required at startup when vehicle(s) do not having charging priority
+                    
                     if (vehicles.Count > 0)
                     {
                         tasks.Add(Task.Run(() => RefreshVehicleChargePriority(vehicles, 30), tokenSource.Token));
                     }
 
                     tasks.Add(Task.Run(() => RefreshNetPowerProduction(vehicles, 1), tokenSource.Token));
-
                     tasks.Add(Task.Run(() => DetermineMonitorCharging(vehicles, 61)));
 
                     //wait until all the tasks in the list are completed
